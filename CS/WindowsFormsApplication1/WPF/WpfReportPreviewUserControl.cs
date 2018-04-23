dd1da367dff7e600e191fd0c123d5c900166c6ac
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication1/WPF/WpfReportPreviewUserControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using DevExpress.XtraReports;
+
+namespace WindowsFormsApplication1.WPF {
+    public partial class WpfReportPreviewUserControl : UserControl {
+        public IReport Report {
+            get { return wpfReportPreviewControl1.Report; }
+            set { wpfReportPreviewControl1.Report = value; }
+        }
+        public WpfReportPreviewUserControl() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VB/WindowsFormsApplication1/Helper.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.IO
+Imports System.Reflection
+Imports DevExpress.XtraReports.UI
+
+Namespace Common
+	Public Module Helper
+		<System.Runtime.CompilerServices.Extension> _
+		Public Function Clone(ByVal report As XtraReport) As XtraReport
+			Using ms = New MemoryStream()
+				report.SaveLayoutToXml(ms)
+				ms.Position = 0
+				Return XtraReport.FromStream(ms, True)
+			End Using
+		End Function
+		<System.Runtime.CompilerServices.Extension> _
+		Public Function GetProperty(Of T)(ByVal obj As Object, ByVal propertyName As String) As T
+			Dim field = obj.GetType().GetProperty(propertyName, BindingFlags.Instance Or BindingFlags.NonPublic)
+			Return CType(field.GetValue(obj, Nothing), T)
+		End Function
+	End Module
+End Namespace
